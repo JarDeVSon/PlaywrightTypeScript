661fee630f6ecfe899c536d51d143f174e0eb0d7
JarDeVSon <jardounifanor@gmail.com>
2023-10-28 13:04:42 -0300

    lets go

--- tests/test-1.spec.ts
+++ tests/test-1.spec.ts
@@ -3,10 +3,10 @@ import { test, expect } from '@playwright/test';
 test('test codegen', async ({ page }) => {
   await page.goto('/my-account');
   await page.getByLabel('Email address *', { exact: true }).click();
-  await page.getByLabel('Email address *', { exact: true }).fill('dalhePlaywright@bol.com.br');
+  await page.getByLabel('Email address *', { exact: true }).fill('dalhePlaywright123@bol.com.br');
   await page.getByLabel('Email address *', { exact: true }).press('Tab');
   await page.locator('#reg_password').click();
   await page.locator('#reg_password').fill('Password@LL$4312121212');
-  await page.getByRole('button', { name: 'REGISTER' }).click();
+  await page.getByRole('button', { name: 'Register' }).click();
   await page.getByRole('link', { name: 'Sign out' }).click();
 });
