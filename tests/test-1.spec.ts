b21717a8e106a6e4972d890cb3606eb273b882c2
JarDeVSon <jardounifanor@gmail.com>
2023-10-28 16:04:29 -0300

    update name button selector

--- tests/test-1.spec.ts
+++ tests/test-1.spec.ts
@@ -1,12 +1,12 @@
 import { test, expect } from '@playwright/test';
 
-test('test codegen', async ({ page }) => {
+test('test using npx playwright codegen feature', async ({ page }) => {
   await page.goto('/my-account');
   await page.getByLabel('Email address *', { exact: true }).click();
-  await page.getByLabel('Email address *', { exact: true }).fill('dalhePlaywright123@bol.com.br');
+  await page.getByLabel('Email address *', { exact: true }).fill('dalhePlaywright12345@bol.com.br');
   await page.getByLabel('Email address *', { exact: true }).press('Tab');
   await page.locator('#reg_password').click();
   await page.locator('#reg_password').fill('Password@LL$4312121212');
-  await page.getByRole('button', { name: 'Register' }).click();
+  await page.getByRole('button', { name: 'register' }).click();
   await page.getByRole('link', { name: 'Sign out' }).click();
 });
