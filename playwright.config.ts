91e2111aad867db874a2d908650aa2eeac52fe1e
JarDeVSon <jardounifanor@gmail.com>
2023-10-28 17:03:07 -0300

    update url under test login form

--- playwright.config.ts
+++ playwright.config.ts
@@ -28,7 +28,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: process.env.URL = 'https://practice.automationtesting.in',
+    baseURL: process.env.URL = 'https://the-internet.herokuapp.com',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
