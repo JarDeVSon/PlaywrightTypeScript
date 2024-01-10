1c5e82c28136741dfe4205e619fd01afdc79ecb0
JarDeVSon <jardounifanor@gmail.com>
2024-01-10 20:02:06 -0300

    undefined configs workers and fullyParallel: false

--- playwright.config.ts
+++ playwright.config.ts
@@ -15,13 +15,13 @@ export default defineConfig({
   /* Timeout global*/
   timeout: 30000,
   /* Run tests in files in parallel */
-  fullyParallel: true,
+  fullyParallel: false,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 1: 1,
+  retries: process.env.CI ? 0 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 3 : 3,
+  workers: process.env.CI ? undefined : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: process.env.CI ? 'github': [['html'], ['json', { outputFile: 'results/results.xml' }]], 
 
